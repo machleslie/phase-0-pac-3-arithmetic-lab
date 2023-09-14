ab9870b6ab97b85bc7fcdcca4e25394bab5b14d7
machleslie <machariales@gmail.com>
2023-09-14 09:12:05 +0300

    Completed assignment

---
+++ index.js
@@ -0,0 +1,42 @@
+let number=5;
+function add(){
+    return(number++);
+}
+function subtract(){
+    return(number--);
+}
+ function multiply(){
+    return(number*6);
+    
+ }
+ function divide(){
+    return(number/5);
+ }
+ 
+ 
+ function increment(n){
+    return(n+1);
+    
+ }
+ function decrement(n){
+    return (n-1);
+ }
+ function add(a,b){
+    return(a+b);
+ }
+ function subtract(a,b){
+    return(a-b);
+    
+ }
+ function multiply(a,b){
+    return(a*b);
+ }
+ function divide(a,b){
+    return(a/b);
+ }
+ function makeInt(n){
+    return(parseInt(n,10));
+ }
+ function preserveDecimal(n){
+    return(parseFloat(n));
+ }
